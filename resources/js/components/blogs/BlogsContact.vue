4dde2f41528cf8e78901baf0e42cb3dfe26e1c15
Freddy Alcarazo <freddyalcarazo@hotmail.com>
2022-04-19 16:24:58 -0500

    basic example

---
+++ resources/js/components/blogs/BlogsContact.vue
@@ -0,0 +1,26 @@
+<template>
+
+        <h1>Blog Contact</h1>
+         <router-link :to="{ name: 'blogs.index' }" class="btn btn-primary">Index</router-link>
+
+</template>
+
+
+<script>
+import { reactive } from "vue";
+
+export default {
+
+    setup(){
+         const contact = reactive({
+            'name': '',
+            'email': '',
+           
+        })
+
+        return {
+            contact
+        }
+    }
+}
+</script>
---
+++ resources/js/components/blogs/BlogsIndex.vue
@@ -0,0 +1,27 @@
+<template>
+    
+     <h1>Blogs Index</h1>
+    <router-link :to="{ name: 'blogs.contact' }" class="btn btn-primary">Contacto</router-link>
+
+
+</template>
+
+
+<script>
+import { reactive } from "vue";
+
+export default {
+
+    setup(){
+         const contact = reactive({
+            'name': '',
+            'email': '',
+           
+        })
+
+        return {
+            contact
+        }
+    }
+}
+</script>
